e631d2bff96a50d485150edc450d3d27506f0157
Surendra Singh Rathore <jpr_surendra@yahoo.co.in>
2022-06-18 23:29:25 +0530

    Code updated

--- Currency-converter/src/main/java/com/myshoppingcart/currencyconverter/web/ForexRateController.java
+++ Currency-converter/src/main/java/com/myshoppingcart/currencyconverter/web/ForexRateController.java
@@ -3,33 +3,42 @@ package com.myshoppingcart.currencyconverter.web;
 import com.myshoppingcart.currencyconverter.entities.*;
 import com.myshoppingcart.currencyconverter.services.*;
 
+import com.myshoppingcart.currencyconverter.vos.CurrencyConversionVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/forex-rate")
+@RequestMapping("/")
 @Slf4j
 public class ForexRateController {
     @Autowired
     private ForexRateService forexRateService;
 
-//    @PostMapping("/")
-//    public ForexRate saveForexRate(@RequestBody ForexRate department) {
-//        log.info("Inside saveDepartment method of DepartmentController");
-//        return  forexRateService.saveDepartment(department);
-//    }
-//
-//    @GetMapping("/")
-//    public List< ForexRate > getAllEmployees() {
-//        return forexRateService.findAll();
-//    }
 
-//    @GetMapping("/{id}")
-//    public Department findDepartmentById(@PathVariable("id") Long departmentId) {
-//        log.info("Inside findDepartmentById method of DepartmentController");
-//        return departmentService.findDepartmentById(departmentId);
-//    }
+
+    @GetMapping("forex-rate/")
+    public List< ForexRate > findAllForexRate() {
+        log.info("Inside findAllForexRate method of ForexRateController");
+        return forexRateService.findAllForexRate();
+    }
+
+    @GetMapping("forex-rate/{currencyCode}")
+    public ForexRate findForexRateByCurrencyCode(@PathVariable("currencyCode") String currencyCode) {
+        log.info("Inside findForexRateById method of ForexRateController");
+        return forexRateService.findForexRateByCurrencyCode(currencyCode);
+    }
+
+    @GetMapping("currency-conversion/{currencyCodeFrom}/{currencyCodeTo}/{amount}")
+    public CurrencyConversionVO currencyConversion(@PathVariable("currencyCodeFrom") String currencyCodeFrom, @PathVariable("currencyCodeTo") String currencyCodeTo, @PathVariable("amount") BigDecimal amount) {
+        log.info("Inside findForexRateById method of ForexRateController");
+        CurrencyConversionVO vo = new CurrencyConversionVO();
+        vo.setFromCurrencyCode(currencyCodeFrom);
+        vo.setToCurrencyCode(currencyCodeTo);
+        vo.setToAmount(amount);
+        return forexRateService.currencyConversion(vo);
+    }
 }
